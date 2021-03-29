836c448bc03e1e829d12624541d8346996c81bec
angelinajeca <jeca.angelina@gmail.com>
2021-03-28 22:27:45 -0400

    update

--- scripts/accueil.js
+++ scripts/accueil.js
@@ -8,4 +8,44 @@ const swiper = new Swiper('.swiper-container', {
         clickable: true,
         bulletActiveClass: 'bullet-active',
         },
-  });
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+const sectionList = document.querySelectorAll('section');
+
+sectionList.forEach(section => {
+    const textes = section.querySelector('h3');
+    const cartes  = section.querySelector('.cards');
+    const carrousel  = section.querySelector('.swiper-container');
+    const video  = section.querySelector('.video');
+
+
+    gsap.timeline({
+        scrollTrigger:{
+            markers: true,
+            start:'top bottom',
+            trigger: section, 
+        }
+    })
+    .from(cartes,{
+        scale: 0.8,
+        y: 100,
+        opacity: 0,
+    })
+    .from(textes,{
+        x: -100,
+        opacity: 0,
+    })
+   
+    .from(video,{
+        scale: 0.8,
+        y: 100,
+        opacity: 0,
+    })
+    .from(carrousel,{
+        scale: 0.8,
+        y: 100,
+        opacity: 0,
+    })
+});
