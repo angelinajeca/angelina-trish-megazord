6437acfa992ab4fcc5ad86eeb34e36cd04cb378d
Trish Nguyen <70410218+Trishng@users.noreply.github.com>
2021-03-28 23:16:18 -0400

    scrollTrigger

--- scripts/discographie.js
+++ scripts/discographie.js
@@ -8,3 +8,40 @@ const swiper = new Swiper('.swiper-container', {
     loop: true,
     allowTouchMove: false,
 });
+
+gsap.registerPlugin(ScrollTrigger);
+
+const sectionList = document.querySelectorAll('section');
+
+sectionList.forEach(section => {
+const card = section.querySelector('.card');
+const title = section.querySelector('h3');  
+const video = section.querySelector('.video');
+
+gsap.timeline({
+  scrollTrigger: {
+    markers: false,
+    start: 'top 75%',
+    end: 'bottom 15%',
+    trigger: section,
+    toggleActions: 'play none resume reverse',
+  }
+})
+
+.from(card,{
+  scale: 0.8,
+  y: 100,
+  opacity: 0,
+})
+.from(title,{
+  x: -100,
+  opacity: 0,
+})
+
+.from(video,{
+  scale: 0.8,
+  y: 100,
+  opacity: 0,
+})
+
+});
